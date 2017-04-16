d6e92334eb040fe43f94de9813270a74c09ecf5b
lqyu_773 <lqyu_773@163.com>
2017-04-16 10:22:49 +0800

    'init'

---
+++ src/main/java/cn/com/strongunion/JenkinsDemo1Application.java
@@ -0,0 +1,12 @@
+package cn.com.strongunion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDemo1Application.class, args);
+	}
+}
