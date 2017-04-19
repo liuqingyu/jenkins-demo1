318b8eb2863ef6fd7127925aecb1d0f56c3481d8
lqyu_773 <lqyu_773@163.com>
2017-04-19 12:37:32 +0800

    test

--- src/main/java/cn/com/strongunion/JenkinsDemo1Application.java
+++ src/main/java/cn/com/strongunion/JenkinsDemo1Application.java
@@ -9,4 +9,12 @@ public class JenkinsDemo1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDemo1Application.class, args);
 	}
+
+	public int sum(int a, int b) {
+		return a + b;
+	}
+
+	public int count(int count) {
+		return count ++ ;
+	}
 }
---
+++ src/main/java/cn/com/strongunion/TaskController.java
@@ -0,0 +1,10 @@
+package cn.com.strongunion;
+
+/**
+ * Created by Administrator on 2017/4/19.
+ */
+public class TaskController {
+    public void demo() {
+        System.out.println("hello");
+    }
+}
